26e3d946bb84680f6b73c6898a0a737c87d1d3c7
Whaletrucker Reef Architect <scutua.tua@icloud.com>
2025-11-20 19:14:11 +0700

    index.ts

--- index.ts
+++ index.ts
@@ -2,3 +2,7 @@ console.log(
   "🔐 GitHub token injected: " +
   process.env.GITHUB_TOKEN?.slice(0, 6) + "..."
 )
+import { safeLogger } from "./utils/safeLogger"
+
+safeLogger("GitHub Token", process.env.GITHUB_TOKEN)
+safeLogger("GitHub Username", process.env.GITHUB_USERNAME)
